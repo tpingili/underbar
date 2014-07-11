9fe68522a7b136efa03270af1d591345abf1b70d
Tejaswini Pingili <tpingili@users.noreply.github.com>
2014-07-11 12:52:45 -0500

    Implemented every function

--- src/underbar.js
+++ src/underbar.js
@@ -203,6 +203,16 @@ var _ = {};
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+      return _.reduce(collection, function(matchesTest, item){
+      if(matchesTest == false)
+        return false;
+      if (iterator == undefined)
+          return item;
+      var result = iterator(item);
+      if(result == false || result == null || result == undefined)
+        return false;
+      return true;
+    }, true)
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
