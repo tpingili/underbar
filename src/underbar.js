c305a322cc92516d5639ed16624632b204eb6c51
Tejaswini Pingili <tpingili@users.noreply.github.com>
2014-07-14 11:06:05 -0500

    Refined _.filter and _.reject

--- src/underbar.js
+++ src/underbar.js
@@ -81,11 +81,9 @@ var _ = {};
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
     var resultArray = [];
-    //if no extra arguments are given, testResult is set to true, else false. A tweak for using it in reject
-    var testResult = (arguments[2] === undefined)? true: false;
-    //pushing all the elements(into a new array) where the test returns a value equal to testResult
+    //pushing all the elements(into a new array) where the test returns true
     _.each(collection, function(item){
-      if(test(item) === testResult)
+      if(test(item))
         resultArray.push(item);
     });
     return resultArray;
@@ -95,7 +93,9 @@ var _ = {};
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    return _.filter(collection, test, false);
+    return _.filter(collection, function(item){
+      return !(test(item));
+    });
   };
 
   // Produce a duplicate-free version of the array.
